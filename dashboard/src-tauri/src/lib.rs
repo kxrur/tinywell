ede297e4f992781425ce750690b612d73d55eec2
kxr <kxrury@gmail.com>
2026-05-23 02:05:39 -0400

    setup specta ts bindings

--- dashboard/src-tauri/src/lib.rs
+++ dashboard/src-tauri/src/lib.rs
@@ -1,14 +1,45 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use std::sync::Mutex;
+
+use specta_typescript::Typescript;
+use tauri::Manager;
+use tauri_specta::*;
+
+mod state;
+
+use crate::state::AppState;
+
 #[tauri::command]
+#[specta::specta]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+let builder = Builder::<tauri::Wry>::new().commands(collect_commands![
+    greet
+    ]);
+
+    #[cfg(debug_assertions)] // <- Only export on non-release builds
+    builder
+        .export(Typescript::default(), "../src/bindings.ts")
+        .expect("Failed to export typescript bindings");
+
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(builder.invoke_handler())
+        .setup(move |app| {
+            app.manage(Mutex::new(AppState::default()));
+
+            builder.mount_events(app);
+
+            //init_database(app.app_handle())?;
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ dashboard/src-tauri/src/state.rs
@@ -0,0 +1,5 @@
+
+#[derive(Default)]
+pub struct AppState {
+    pub db_path: String,
+}
